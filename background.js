f0c83526cb36cc6ec70d8bea768b81e6f647229a
Maximiliano Morero <maxcutecats@gmail.com>
2022-09-19 15:02:51 -0300

    Custom MakeElement class & code simplification

--- background.js
+++ background.js
@@ -4,7 +4,14 @@ const CUSTOM_ID = "matzi-yt-short-controller";
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
 
-        if (!tab.url.startsWith("https://www.youtube.com/shorts")) return;
+        if (!tab.url.startsWith("https://www.youtube.com/shorts")) {
+            
+            if (execScript(tab.id, removeController)) {
+                removeController();
+            }
+
+            return;
+        }
 
         injectScript(tab.id);
         injectController(tab.id);
@@ -48,7 +55,6 @@ async function initialize(t) {
     }
     
     execScript(tab.id, script);
-    execScript(tab.id, addController);
     execScript(tab.id, injectCss, chrome.runtime.getURL("controller-style.css"));
 
 }
@@ -90,243 +96,6 @@ async function getTab() {
     return res;
 
 }
-
-function addController() { //working on page context
-
-    if (document.getElementById("matzi-yt-short-controller")) return;
-
-    const element = document.createElement("div");
-    const mover = document.createElement("div");
-    document.body.appendChild(element);
-    element.appendChild(mover);
-    document.body.style.height = "100vh";
-
-    element.id = "matzi-yt-short-controller"; 
-    element.classList.add("matzi")
-
-    mover.id = "matzi-yt-mover";
-    mover.classList.add("matzi");
-
-    addElements();
-
-    let mX, mY;
-
-    mover.addEventListener("mousedown", dragMouseDown);
-
-    function dragMouseDown(e) {
-        mX = e.clientX;
-        mY = e.clientY;
-
-        document.addEventListener("mouseup", dragMouseUp);
-        document.addEventListener("mousemove", elementDrag);
-
-        document.body.style.cursor = "grabbing";
-        mover.style.cursor = "grabbing";
-    }
-
-    function elementDrag(e) {
-
-        const newY = (element.offsetTop - (mY - e.clientY));
-        const newX = (element.offsetLeft - (mX - e.clientX));
-    
-        if (newY > 0 && newY + 222 < document.body.clientHeight) element.style.top = newY + "px";
-        
-        if (newX > 0 && newX + 252 < document.body.clientWidth) element.style.left = newX + "px";
-    
-    
-        mX = e.clientX;
-        mY = e.clientY;
-    }
-
-    function dragMouseUp() {
-        document.removeEventListener("mouseup", dragMouseUp);
-        document.removeEventListener("mousemove", elementDrag);
-
-        document.body.style.cursor = "auto";
-        mover.style.cursor = "grab";
-    }
-
-    function addElements() {
-
-        const buttonsDiv = document.createElement("div");
-        const timeDiv = document.createElement("div");
-
-        element.append(buttonsDiv, timeDiv); //add the elements
-
-        buttonsDiv.classList.add("matzi", "buttons-div");
-        timeDiv.classList.add("matzi","time-div");
-
-        addTimeCounter(timeDiv);
-        inputElements(timeDiv);
-
-    }
-
-    function inputElements(elem) {
-        const input = document.createElement("input");
-        let video = document.querySelectorAll("video")[1];
-        const timeText = document.getElementById("matzi-actualtime");
-        const durationText = document.getElementById("matzi-totaltime");
-
-        console.log(timeText);
-
-        input.type = "range";
-        input.min = "0";
-        input.max = "100";
-        input.step = "1";
-        input.value = "0";
-
-        input.id = "matzi-time-input";
-        input.classList.add("matzi");
-        elem.appendChild(input);
-        
-        try {
-            video.addEventListener("timeupdate", addTimer);
-        } catch (e) {
-            const container = document.getElementById("page-manager");
-
-            const observer = new MutationObserver((mutations) => {
-                mutations.forEach(mutation => {
-                    if (document.querySelectorAll("video")[1]) 
-                    {
-                        video = document.querySelectorAll("video")[1];
-                        video.addEventListener("timeupdate", addTimer);
-                        observer.disconnect();
-                    }
-                })
-            })
-
-            observer.observe(container, {childList: true, subtree: true});
-        }
-
-        input.addEventListener("input", (e) => {
-            if (!video) {
-                video = document.querySelectorAll("video")[1];
-                video.removeEventListener("timeupdate", addTimer);
-                video.addEventListener("timeupdate", addTimer);
-            }
-    
-                updateVideo(input);
-                timeText.innerText = Math.floor(video.currentTime);
-             
-        })
-
-        function addTimer() {
-            let time = video.currentTime;
-            let percentage = time / video.duration;
-
-            input.value = input.max * percentage;
-            timeText.innerText = Math.floor(video.currentTime);
-            durationText.innerText = parseInt(video.duration) || 0;
-        }
-    }
-
-    /**
-     * 
-     * @param {HTMLDivElement} elem 
-     */
-    function addTimeCounter(elem) {
-        const container = document.createElement("div");
-        const actualTime = document.createElement("p");
-        const totalTime = document.createElement("p");
-
-        elem.appendChild(container);
-        container.classList.add("time-display");
-
-        container.append(actualTime, totalTime);
-
-        actualTime.id = "matzi-actualtime";
-        totalTime.id = "matzi-totaltime";
-
-        actualTime.contentEditable = true;
-
-        actualTime.addEventListener("focusin", (e) => {
-            videoPause();
-            const lastTime = parseFloat(actualTime.textContent);
-
-            actualTime.addEventListener("focusout", handleFocus)
-            document.addEventListener("keydown", handleKeys)
-
-            /**
-             * @param {FocusEvent} e
-             */
-            function handleFocus(e) {
-                if (lastTime === parseFloat(actualTime.textContent)) return;
-
-                const settedValue = parseInt(actualTime.textContent);
-                const duration = getVideoDuration();
-
-                settedValue < duration ? updateVideoByTime(settedValue) : actualTime.textContent = lastTime;
-
-                actualTime.removeEventListener("focusout", handleFocus);
-                document.removeEventListener("keydown", handleKeys)
-                videoPlay();
-            }
-
-            /**
-             * 
-             * @param {KeyboardEvent} e 
-             */
-            function handleKeys(e) {
-                if (e.key == " ") {
-                    e.preventDefault(true);
-                }
-
-                if (e.key == "Enter") {
-                    e.preventDefault(true);
-                    actualTime.blur();
-                }
-            }
-        })
-    }
-
-    /**
-     * Change video duration from a input type range
-     * @param {HTMLInputElement} input 
-     */
-    function updateVideo(input) {
-        const video = document.querySelectorAll("video")[1];
-
-        let min = input.min;
-        let max = input.max;
-        let value = input.value;
-
-        video.currentTime = ((value - min) * 100 / (max - min)) / 100 * video.duration;
-    }
-
-    /**
-     * Change video from a number (seconds)
-     * @param {Number} time 
-     */
-    function updateVideoByTime(time) {
-        const video = document.querySelectorAll("video")[1];
-
-        if (time > video.duration) { 
-            video.currentTime = video.duration;
-            return;
-        }
-
-        video.currentTime = time;
-    }
-
-    function getVideoDuration() {
-        const video = document.querySelectorAll("video")[1];
-
-        return video.duration;
-    }
-
-    function videoPause() {
-        const video = document.querySelectorAll("video")[1];
-
-        video.pause();
-    }
-
-    function videoPlay() {
-        const video = document.querySelectorAll("video")[1];
-
-        video.play();
-    }
-
-} //working on page context
 
 function removeController() {
     document.getElementById("matzi-yt-short-controller").remove();
@@ -378,4 +147,9 @@ function injectController(id) {
 
     execScript(id, script, chrome.runtime.getURL('controller.js'));
 
+}
+
+function isControllerUp() {
+    if (document.querySelector("#matzi-yt-short-controller")) return true;
+    else return false;
 }
--- controller.js
+++ controller.js
@@ -1,20 +1,25 @@
-class Controller {
+class ControllerBase {
 
     #video
 
     constructor() {
         this.#init();
 
-        document.addEventListener("videoChanged", this.updateControls);
+        document.addEventListener("videoChanged", this.updateValues);
     }
 
     #init() {
         const observer = new MutationObserver(mutations => {
             mutations.forEach(mutation => {
-                if (document.querySelector("video.video-stream.html5-main-video") && !this.#video) {
-                    this.#video = document.querySelector("video.video-stream.html5-main-video");
+                const vid = document.querySelector("video.video-stream.html5-main-video");
+                if (vid && !this.#video) {
+
+                    if (isNaN(vid.duration)) return;
+
+                    this.#video = true;
+                    this.updateValues();
+
                     observer.disconnect();
-                    return;
                 }
             })
         })
@@ -22,20 +27,158 @@ class Controller {
         observer.observe(document.body, {childList: true, subtree: true});
     }
 
+    updateValues() {}
+
+}
+
+class Controller extends ControllerBase {
+
+    #input
+    #mainContainer
+    #mover
+    #actualTime
+    #duration
+
+    constructor() {
+        super();
+        this.#assemble();
+    }
+
+    #assemble() {
+        let timeInput;
+        let mover;
+        let actual;
+        let total;
+        let mainContainer;
+
+        mainContainer = new MakeElement("div", {id: "matzi-yt-short-controller", class: ["matzi"]});
+        
+        document.body.append(mainContainer);
+        mainAssemble();
+        moverBehavoiur();
+        
+        function mainAssemble() {
+            mover = new MakeElement("div", {id: "matzi-yt-mover", class: ["matzi"]})
+            const topDiv = new MakeElement("div", {class: ["matzi", "buttons-div"]})
+            const timeContainer = new MakeElement("div", {class: ["matzi", "time-div"]})
+
+            mainContainer.append(mover, topDiv, timeContainer);
+            timerAssemble(timeContainer);
+        }
+
+        function timerAssemble(parent) {
+            const timeShower = new MakeElement("div", {class: ["time-display"]})
+            timeInput = new MakeElement("input", {id: "matzi-time-input", class: ["matzi"]})
+
+            parent.append(timeShower, timeInput);
+
+            timeInput.type = "range";
+            timeInput.min = 0;
+            timeInput.max = 100;
+            timeInput.step = 1;
+
+            timeNumbersAssemble(timeShower);
+        }
+
+        function timeNumbersAssemble(parent) {
+            actual = new MakeElement("p", {id: "matzi-actualtime"})
+            total = new MakeElement("p", {id: "matzi-totaltime"})
+
+            parent.append(actual, total);
+
+            actual.contentEditable = true;
+        }
+
+        function moverBehavoiur() {
+
+            let mX, mY;
+            mover.addEventListener("mousedown", dragMouseDown);
+
+            function dragMouseDown(e) {
+                mX = e.clientX;
+                mY = e.clientY;
+        
+                document.addEventListener("mouseup", dragMouseUp);
+                document.addEventListener("mousemove", elementDrag);
+        
+                document.body.style.cursor = "grabbing";
+                mover.style.cursor = "grabbing";
+            }
+        
+            function elementDrag(e) {
+        
+                const newY = (mainContainer.offsetTop - (mY - e.clientY));
+                const newX = (mainContainer.offsetLeft - (mX - e.clientX));
+            
+                if (newY > 0 && newY + 222 < document.body.clientHeight) mainContainer.style.top = newY + "px";
+                
+                if (newX > 0 && newX + 252 < document.body.clientWidth) mainContainer.style.left = newX + "px";
+            
+            
+                mX = e.clientX;
+                mY = e.clientY;
+            }
+        
+            function dragMouseUp() {
+                document.removeEventListener("mouseup", dragMouseUp);
+                document.removeEventListener("mousemove", elementDrag);
+        
+                document.body.style.cursor = "auto";
+                mover.style.cursor = "grab";
+            }
+
+        }
+    }
+
+}
+
+/**
+ * @returns {HTMLElement}
+ */
+class MakeElement {
+
+    #id
+    #classes
+    #element
+
     /**
      * 
-     * @param {HTMLVideoElement} video 
+     * @param {Element} kind 
+     * @param {object} object
+     * @example new MakeElement("div", {id:"newElement", class: ["aClass", "anotherClass"]})
      */
-    update(video) {
-        this.#video = video;
-    }
+    constructor(kind, object) {
+        const spaceError = new Error("No spaces allowed");
+        if (!kind) throw new Error("Kind of element required");
+        if (kind.includes(" ")) throw spaceError;
 
-    updateControls() {
-        console.log("Updated controls!");
-    }
+        try {
+            this.#element = document.createElement(kind);
+        } catch (e) {
+            console.error(e)
+            return;
+        }
 
-    getVideo() {
-        return this.#video;
+        if (object) {
+            if (object.id) {
+                this.#id = object.id;
+                if (typeof this.#id !== "string") throw new Error("Id must be a string");
+                if (this.#id.includes(" ")) throw spaceError;
+
+                this.#element.id = this.#id;
+            }
+            if (object.class) {
+                this.#classes = object.class;
+                if (typeof this.#classes !== "object") throw new Error("Class must be entered as array")
+                this.#classes.forEach(cls => {
+                    if (cls.includes(" ")) throw spaceError;
+                    this.#element.classList.add(cls);
+                });
+
+            }
+        }
+
+        return this.#element;
     }
 
 }
